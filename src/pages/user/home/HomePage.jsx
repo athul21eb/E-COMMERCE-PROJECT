081716eb6f5002bb34d720ad5db0300550fb4fd7
athul <athul.eb21@gmail.com>
2024-11-29 23:20:05 +0530

    env production mode

--- src/pages/user/home/HomePage.jsx
+++ src/pages/user/home/HomePage.jsx
@@ -11,44 +11,46 @@ import { useEffect } from "react";
 
 const fetchNewArrivals = async () => {
   try {
-    const { data } = await axios.get(`http://localhost:${import.meta.env.VITE_SERVER_PORT}/v1/public//new-arrivals?limit=10`);
+    const { data } = await axios.get(
+      `${
+        import.meta.env.VITE_FRONTEND_ENV === "development"
+          ? import.meta.env.VITE_DEVELOPMENT_BACKEND_URL
+          : import.meta.env.VITE_PRODUCTION_BACKEND_URL
+      }/v1/public//new-arrivals?limit=10`
+    );
     return data; // Return valid data
   } catch (error) {
     // Log the error and rethrow it for React Query to catch
     console.error("Error fetching new arrivals:", error);
-    throw new Error(error.response?.data?.message || "Failed to fetch new arrivals");
+    throw new Error(
+      error.response?.data?.message || "Failed to fetch new arrivals"
+    );
   }
 };
 
-
 export default function Homepage() {
-  const { 
-    data = { newArrivals: [], mostDelivered: [], mostLoved: [],banners:[] }, 
-    isLoading, 
-   
+  const {
+    data = { newArrivals: [], mostDelivered: [], mostLoved: [], banners: [] },
+    isLoading,
   } = useQuery({
-    queryKey: ["newArrivals"],   // Query key
-    queryFn: fetchNewArrivals,  // Fetch function
+    queryKey: ["newArrivals"], // Query key
+    queryFn: fetchNewArrivals, // Fetch function
     refetchOnWindowFocus: false, // Disable refetch on window focus
-    retry: 2,                   // Retry fetching twice before failing
+    retry: 2, // Retry fetching twice before failing
   });
-
-  
 
   if (isLoading) {
     return <LoadingFullScreen />;
   }
 
- 
-
-  const { newArrivals, mostDelivered, mostLoved ,banners} = data;
+  const { newArrivals, mostDelivered, mostLoved, banners } = data;
 
   return (
     <div className="px-4 sm:px-6 lg:px-8">
-     { banners&&banners?.length&&<Carousel items={banners} />}
+      {banners && banners?.length && <Carousel items={banners} />}
       <CategoryBanner />
 
-      {newArrivals&&newArrivals.length > 0 && (
+      {newArrivals && newArrivals.length > 0 && (
         <>
           <motion.div
             initial={{ opacity: 0, y: -50 }}
@@ -60,7 +62,9 @@ export default function Homepage() {
             <h2 className="text-3xl lg:text-5xl font-extrabold tracking-tight text-gray-900">
               New <span className="text-gray-500">Arrivals</span>
             </h2>
-            <p className="text-base lg:text-lg text-gray-500 mt-3">Grab the new Ones</p>
+            <p className="text-base lg:text-lg text-gray-500 mt-3">
+              Grab the new Ones
+            </p>
           </motion.div>
 
           <ProductList productData={newArrivals} />
@@ -73,7 +77,11 @@ export default function Homepage() {
               viewport={{ once: true, margin: "-100px" }}
               className="mb-6 md:mb-0"
             >
-              <MostProducts productData={mostLoved} Heading="Most Loved" bgcolor="red" />
+              <MostProducts
+                productData={mostLoved}
+                Heading="Most Loved"
+                bgcolor="red"
+              />
             </motion.div>
 
             <motion.div
@@ -82,7 +90,11 @@ export default function Homepage() {
               transition={{ duration: 0.5, ease: "easeOut" }}
               viewport={{ once: true, margin: "-100px" }}
             >
-              <MostProducts productData={mostDelivered} Heading="Most Delivered" bgcolor="blue" />
+              <MostProducts
+                productData={mostDelivered}
+                Heading="Most Delivered"
+                bgcolor="blue"
+              />
             </motion.div>
           </div>
         </>
